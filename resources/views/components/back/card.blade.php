687dca50861fb03da3ca079e451d6879c4bdaf27
magerishere <immagerishere@gmail.com>
2023-06-06 16:44:24 +0430

    add session message to card for more manageable form component inside table

--- resources/views/components/back/card.blade.php
+++ resources/views/components/back/card.blade.php
@@ -1,4 +1,9 @@
 <div class="card">
+    @if($sessionMessage = session()->get('session_message'))
+        <div class="alert alert-{{session()->get('session_message_type')}}">
+            {{$sessionMessage}}
+        </div>
+    @endif
     <!-- /.card-header -->
     <div class="card-body">
         {{$slot}}
--- resources/views/components/back/form.blade.php
+++ resources/views/components/back/form.blade.php
@@ -1,10 +1,4 @@
-@if($sessionMessage = session()->get('session_message'))
-    <div class="alert alert-{{session()->get('session_message_type')}}">
-        {{$sessionMessage}}
-    </div>
-@endif
 <form action="{{$attributes->get('action')}}" method="{{$attributes->get('method','POST')}}"
-
       @if($hasFile)
           enctype="multipart/form-data"
     @endif
