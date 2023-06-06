816bab8da769017917a9467d635781a9b60f2fe3
magerishere <immagerishere@gmail.com>
2023-06-06 13:38:37 +0430

    add BackController and FrontController for extending

--- Modules/AdminPanel/Http/Controllers/Back/AdminPanelController.php
+++ Modules/AdminPanel/Http/Controllers/Back/AdminPanelController.php
@@ -2,21 +2,19 @@
 
 namespace Modules\AdminPanel\Http\Controllers\Back;
 
+use App\Http\Controllers\BackController;
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
 
-class AdminPanelController extends Controller
+class AdminPanelController extends BackController
 {
     /**
      * Load admin dashboard
      * @return Application|Factory|View|\Illuminate\Foundation\Application
      */
-   public function dashboard()
-   {
-       return view('adminpanel::back.dashboard');
-   }
+    public function dashboard()
+    {
+        return view('adminpanel::back.dashboard');
+    }
 }
