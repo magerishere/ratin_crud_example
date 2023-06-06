5fb6a3d9058190e82802316c32e0b27d0b9ebc28
magerishere <immagerishere@gmail.com>
2023-06-06 13:34:10 +0430

    load css js dashboard only in self

--- Modules/AdminPanel/Resources/views/back/dashboard.blade.php
+++ Modules/AdminPanel/Resources/views/back/dashboard.blade.php
@@ -1,5 +1,23 @@
 @extends('adminpanel::back.layouts.master')
 
+@push('styles')
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet"
+          href={{asset("back/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css")}}>
+    <!-- iCheck -->
+    <link rel="stylesheet" href={{asset("back/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css")}}>
+    <!-- JQVMap -->
+    <link rel="stylesheet" href={{asset("back/assets/plugins/jqvmap/jqvmap.min.css")}}>
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href={{asset("back/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}>
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href={{asset("back/assets/plugins/daterangepicker/daterangepicker.css")}}>
+    <!-- summernote -->
+    <link rel="stylesheet" href={{asset("back/assets/plugins/summernote/summernote-bs4.min.css")}}>
+
+@endpush
 @section('content')
     <div class="container-fluid">
         <!-- Small boxes (Stat box) -->
@@ -582,3 +600,26 @@
     </div><!-- /.container-fluid -->
 
 @endsection
+
+@push('scripts')
+    <!-- ChartJS -->
+    <script src="{{asset('back/assets/plugins/chart.js/Chart.min.js')}}"></script>
+    <!-- Sparkline -->
+    <script src="{{asset('back/assets/plugins/sparklines/sparkline.js')}}"></script>
+    <!-- JQVMap -->
+    <script src="{{asset('back/assets/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+    <script src={{asset("back/assets/plugins/jqvmap/maps/jquery.vmap.usa.js")}}></script>
+    <!-- jQuery Knob Chart -->
+    <script src={{asset("back/assets/plugins/jquery-knob/jquery.knob.min.js")}}></script>
+    <!-- daterangepicker -->
+    <script src={{asset("back/assets/plugins/moment/moment.min.js")}}></script>
+    <script src={{asset("back/assets/plugins/daterangepicker/daterangepicker.js")}}></script>
+    <!-- Tempusdominus Bootstrap 4 -->
+    <script src={{asset("back/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js")}}></script>
+    <!-- Summernote -->
+    <script src={{asset("back/assets/plugins/summernote/summernote-bs4.min.js")}}></script>
+    <!-- overlayScrollbars -->
+    <script src={{asset("back/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js")}}></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{asset('back/assets/dist/js/pages/dashboard.js')}}"></script>
+@endpush
